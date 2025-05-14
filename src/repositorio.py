fc9c489ae00c20eeda629e1e14da793dba2e84a5
Rsapa604 <markis01pro@gmail.com>
2025-05-14 09:56:29 +0200

    repositorio2

--- src/repositorio.py
+++ src/repositorio.py
@@ -20,3 +20,33 @@ class RepositorioProcesos:
         if pid not in self.procesos:
             raise ValueError("Proceso no encontrado.")
         return self.procesos[pid]
+    def guardar_json(self, ruta):
+        datos = []
+        for p in self.procesos.values():
+            datos.append({
+                "pid": p.pid,
+                "duracion": p.duracion,
+                "prioridad": p.prioridad
+            })
+        with open(ruta, "w") as f:
+            import json
+            json.dump(datos, f, indent=2)
+
+    def cargar_json(self, ruta):
+        with open(ruta, "r") as f:
+            import json
+            datos = json.load(f)
+        self.procesos = {}
+        
+
+    def guardar_csv(self, ruta):
+        with open(ruta, "w") as f:
+            f.write("pid;duracion;prioridad\n")
+            for p in self.procesos.values():
+                f.write(f"{p.pid};{p.duracion};{p.prioridad}\n")
+
+    def cargar_csv(self, ruta):
+        with open(ruta, "r") as f:
+            lines = f.readlines()
+        self.procesos = {}
+        
