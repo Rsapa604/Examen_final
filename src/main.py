60c3e31118028731d5538fb8cc843abed1bfc62e
Rsapa604 <markis01pro@gmail.com>
2025-05-14 09:58:38 +0200

    main1

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from src.proceso import Proceso
+from src.repositorio import RepositorioProcesos
+from src.scheduler import FCFSScheduler, RoundRobinScheduler
+from src.metrics import calcular_metricas
+
+repo = RepositorioProcesos()
