5a37bdb445355698faf578ef84288c8a3b842cad
Rsapa604 <markis01pro@gmail.com>
2025-05-14 09:51:52 +0200

    scheduler2

--- src/scheduler.py
+++ src/scheduler.py
@@ -14,3 +14,17 @@ class FCFSScheduler(Scheduler):
             p.tiempo_fin = tiempo
             gantt.append((p.pid, p.tiempo_inicio, p.tiempo_fin))
         return gantt
+class RoundRobinScheduler(Scheduler):
+    def __init__(self, quantum):
+        if quantum <= 0:
+            raise ValueError("El quantum debe ser un entero positivo.")
+        self.quantum = quantum
+
+    def planificar(self, procesos):
+        copia = []
+        for original in procesos:
+            p = Proceso(original.pid, original.duracion, original.prioridad)
+            copia.append(p)
+
+        tiempo = 0
+        gantt = []
