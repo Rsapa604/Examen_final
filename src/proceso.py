45898a49df8842a57ca5343eb5edd0a1f6667f66
Rsapa604 <markis01pro@gmail.com>
2025-05-14 09:43:21 +0200

    proceso2

--- src/proceso.py
+++ src/proceso.py
@@ -4,3 +4,10 @@ class Proceso:
             raise ValueError("PID no puede estar vacío.")
         if duracion <= 0:
             raise ValueError("Duración debe ser un entero positivo.")
+        self.pid = pid
+        self.duracion = duracion
+        self.prioridad = prioridad
+        self.tiempo_restante = duracion
+        self.tiempo_llegada = 0
+        self.tiempo_inicio = None
+        self.tiempo_fin = None
