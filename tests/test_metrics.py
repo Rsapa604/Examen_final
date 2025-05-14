19d340d2ece314c9f5a3c857f39bd4c8678c3962
Rsapa604 <markis01pro@gmail.com>
2025-05-14 10:08:40 +0200

    test_metrics2

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,3 +1,18 @@
 import unittest
 from src.proceso import Proceso
 from src.metrics import calcular_metricas
+
+class TestMetrics(unittest.TestCase):
+    def test_metricas_basicas(self):
+        p1 = Proceso("P1", 4, 1)
+        p1.tiempo_inicio = 0
+        p1.tiempo_fin = 4
+
+        p2 = Proceso("P2", 3, 2)
+        p2.tiempo_inicio = 4
+        p2.tiempo_fin = 7
+
+        resultados = calcular_metricas([p1, p2])
+        self.assertAlmostEqual(resultados["promedio_respuesta"], 2.0)
+        self.assertAlmostEqual(resultados["promedio_retorno"], 7.0 / 2)
+        self.assertAlmostEqual(resultados["promedio_espera"], 1.0)
