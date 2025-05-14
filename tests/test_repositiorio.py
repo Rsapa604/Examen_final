99b1c7cce1a30b696ae34cf28b76a00618ec582f
Rsapa604 <markis01pro@gmail.com>
2025-05-14 10:05:46 +0200

    funciones test_repositorio

--- tests/test_repositiorio.py
+++ tests/test_repositiorio.py
@@ -2,3 +2,33 @@ import unittest
 import os
 from src.proceso import Proceso
 from src.repositorio import RepositorioProcesos
+
+class TestRepositorio(unittest.TestCase):
+    def setUp(self):
+        self.repo = RepositorioProcesos()
+        self.p1 = Proceso("P1", 4, 1)
+        self.repo.agregar(self.p1)
+
+    def test_agregar_duplicado(self):
+        with self.assertRaises(ValueError):
+            self.repo.agregar(Proceso("P1", 3, 2))
+
+    def test_eliminar(self):
+        self.repo.eliminar("P1")
+        self.assertEqual(self.repo.listar(), [])
+
+    def test_guardar_y_cargar_json(self):
+        ruta = "test_procesos.json"
+        self.repo.guardar_json(ruta)
+        nuevo = RepositorioProcesos()
+        nuevo.cargar_json(ruta)
+        self.assertEqual(len(nuevo.listar()), 1)
+        os.remove(ruta)
+
+    def test_guardar_y_cargar_csv(self):
+        ruta = "test_procesos.csv"
+        self.repo.guardar_csv(ruta)
+        nuevo = RepositorioProcesos()
+        nuevo.cargar_csv(ruta)
+        self.assertEqual(len(nuevo.listar()), 1)
+        os.remove(ruta)
